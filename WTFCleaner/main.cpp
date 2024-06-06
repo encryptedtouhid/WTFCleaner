83bd0ca48597b5fc313b1589f37c9351b90cf347
Khaled <me@tuhidulhossain.com>
2024-06-07 02:23:03 +0800

    Added main Function

---
+++ WTFCleaner/main.cpp
@@ -0,0 +1,16 @@
+#include "TempCleaner.h"
+#include <iostream>
+
+int main() {
+    // Get the singleton instance of TempCleaner
+    TempCleaner& cleaner = TempCleaner::getInstance();
+    cleaner.clearTempFolder(TempFolder::WINDOWS_TEMP);
+    cleaner.clearTempFolder(TempFolder::CURRENT_USER_TEMP);
+    cleaner.clearTempFolder(TempFolder::PREFETCH);
+     cleaner.clearTempFolder(TempFolder::COOKIES);
+     cleaner.clearTempFolder(TempFolder::TEMP_INTERNET_FILES);
+     cleaner.clearTempFolder(TempFolder::HISTORY);
+     cleaner.clearTempFolder(TempFolder::JAVA_CACHE);
+
+    return 0;
+}
